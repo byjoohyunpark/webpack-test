b80783192218cf0ae4295122081760fe930c46bc
joohyun park <joohyun.park@havas.com>
2020-01-28 12:37:08 -0500

    juggling eslint prettier config

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import _ from 'lodash';
-import { earth } from './app/earth';
-import { moon } from './app/moon';
-import './style/main.scss'
+import _ from "lodash";
+import { earth } from "./app/earth";
+import { moon } from "./app/moon";
+import "./style/main.scss";
 
 function component() {
-    const element = document.createElement('div');
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
+  const element = document.createElement("div");
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  return element;
 }
 
-document.querySelector('#app').appendChild(component());
-
+document.querySelector("#app").appendChild(component());
 console.log("init index.js");
 
 earth();
 moon();
-
-
