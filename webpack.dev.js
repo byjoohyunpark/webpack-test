c8cc6f6617498932fc4a7bda69283be8ecf8d4ad
joohyun park <jhp527@nyu.edu>
2020-03-18 12:26:15 -0400

    add output dir to dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,17 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const path = require('path')
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'eval-source-map',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, './dist')
+  },
   module: {
     rules: [
       {
