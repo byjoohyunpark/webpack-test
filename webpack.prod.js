ff3f19a42da30f2e97ee1179572ecd0b14f6da9a
joohyun park <jhp527@nyu.edu>
2020-03-18 14:37:11 -0400

    add mini css extract plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
@@ -9,14 +10,24 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.[contenthash:20].js'
   },
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all'
-  //   }
-  // },
-  plugins: [new CleanWebpackPlugin()],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:20].css',
+      chunkFilename: '[id].[contenthash:20].css'
+    })
+  ],
   module: {
     rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
