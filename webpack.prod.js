72dda6d67b3cfd757862ebea8ac92bfe7837ac77
joohyun park <jhp527@nyu.edu>
2020-07-05 16:12:40 -0400

    wip

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 const path = require('path')
 
 module.exports = {
@@ -14,9 +15,7 @@ module.exports = {
     filename: '[name].bundle.[contenthash:20].js'
   },
   // optimization: {
-  //   splitChunks: {
-  //     chunks: 'all'
-  //   }
+  //   usedExports: true
   // },
   plugins: [
     new CleanWebpackPlugin(),
