3573803eccc85f920debf50d4d3aa17e56565f5a
joohyun park <jhp527@nyu.edu>
2020-07-06 14:52:02 -0400

    remove test import

--- src/index2.js
+++ src/index2.js
@@ -2,7 +2,3 @@ import './style/main.scss'
 import './app/Message'
 import './app/Paragraph'
 import './app/Paragraph2'
-// import { cube } from './js/math'
-
-import * as THREE from 'three'
-import * as d3 from 'd3'
