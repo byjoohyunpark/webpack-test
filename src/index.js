4a31405912a26556e95be8b5da1d670b2ecc5328
joohyun park <jhp527@nyu.edu>
2020-07-07 15:32:09 -0400

    clean indexs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-// import './style/main.scss'
-// import './app/Message'
-// import './app/Paragraph'
-
 import { add, subtract, multiply } from './js/utils'
--- src/index2.js
+++ src/index2.js
@@ -1,8 +1,3 @@
-// import './style/main.scss'
-// import './app/Message'
-// import './app/Paragraph'
-// import './app/Paragraph2'
-
 import { add, subtract, multiply } from './js/utils'
 // import * as THREE from 'three'
 // import * as d3 from 'd3'
