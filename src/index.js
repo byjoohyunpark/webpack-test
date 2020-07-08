91bf02f9924974c852d4f79dcbd6e082768b425d
joohyun park <jhp527@nyu.edu>
2020-07-08 14:23:40 -0400

    test treeshaking node modules

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-// import { flow, get } from 'lodash'
 import { add, subtract, multiply } from './js/utils'
+
+// import { flow, get } from 'lodash'
+// import { flow, get } from 'lodash-es'
+// console.log(flow, get)
+
+// import { extent, histogram, mean } from 'd3'
+// import extent from 'd3-array/src/extent'
+// import histogram from 'd3-array/src/histogram'
+// import mean from 'd3-array/src/mean'
+// console.log(extent(), histogram(), mean())
